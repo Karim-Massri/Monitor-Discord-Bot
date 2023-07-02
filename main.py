3c48f1310e6aae7f7c3991419595587edfd313ce
Karim <105588639+Karim-Massri@users.noreply.github.com>
2023-07-02 21:24:17 +1000

    added graph command

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import db
 from datetime import datetime, timedelta
 from discord import Spotify
 import sqlite3
+# Graph
+import matplotlib.pyplot as plt
+import numpy as np
 
 client = discord.Client(intents=discord.Intents.all())
 SERVER_ID = config.SERVER_ID
@@ -226,6 +229,75 @@ async def on_message(message):
         # Close the database connection
         cursor.close()
         conn.close()
+
+    ##########################################################################
+
+    if message.content == "!monitor graph":
+      # Open the database connection
+      conn = sqlite3.connect('discordbot.db')
+      cursor = conn.cursor()
+
+      # Retrieve the user's weekly playtime data from the database
+      query = """
+      SELECT strftime('%Y-%m-%d', start_time) AS play_date, SUM(duration) AS total_playtime
+      FROM user_games
+      WHERE user_id = ? AND start_time >= datetime('now', '-7 days')
+      GROUP BY play_date
+      ORDER BY play_date
+      """
+      user_id = message.author.id  # Use the ID of the user who triggered the command
+      cursor.execute(query, (user_id,))
+      results = cursor.fetchall()
+
+      # Extract the play dates and playtime values
+      dates = [row[0] for row in results]
+      playtimes = [row[1] / 3600 for row in results]  # Convert playtime from seconds to hours
+
+      # Create a complete list of dates for the week
+      start_date = datetime.now() - timedelta(days=6)
+      end_date = datetime.now()
+      all_dates = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(7)]
+
+      # Fill in the playtime values for the days the user has played
+      filled_playtimes = []
+      for date in all_dates:
+          if date in dates:
+              index = dates.index(date)
+              filled_playtimes.append(playtimes[index])
+          else:
+              filled_playtimes.append(0)
+
+      # Plot the data
+      plt.plot(all_dates, filled_playtimes, marker='o')
+      plt.xlabel(f"Date: {start_date.strftime('%d-%m-%Y')} to {end_date.strftime('%d-%m-%Y')}")
+      plt.ylabel('Playtime (hours)')
+      plt.title(f"{message.author.name} Weekly Playtime")
+      plt.xticks(rotation=45)
+      plt.grid(True)
+
+      # Set x-axis labels to weekdays (e.g., Mon, Tue, Wed)
+      weekday_labels = [datetime.strptime(date, '%Y-%m-%d').strftime('%a') for date in all_dates]
+      plt.gca().set_xticklabels(weekday_labels)
+
+      # Set y-axis ticks in intervals of 0.5 hours
+      y_ticks = np.arange(0, max(filled_playtimes) + 0.5, 0.5)
+      plt.yticks(y_ticks)
+
+      # Save the plot as an image file
+      plot_filename = 'playtime_plot.png'
+      plt.savefig(plot_filename, bbox_inches='tight')
+      plt.close()
+
+      # Create a discord.File object from the saved image file
+      file = discord.File(plot_filename)
+
+      # Send the image file as a message attachment
+      await message.channel.send(file=file)
+
+      os.remove(plot_filename)
+      # Close the database connection
+      cursor.close()
+      conn.close()
 
 
 client.run(config.TOKEN)
